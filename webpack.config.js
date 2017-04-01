1b39ee6e3767bdb0d2cfc6cebe9184b749ab4639
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-04-02 00:59:45 +0800

    Upgrade depenencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,20 @@ const webpack = require('webpack');
 const env = process.env.NODE_ENV;
 const config = {
   resolve: {
-    root: ['src'],
-    extensions: ['', '.js', '.jsx'],
-    moduleDirectories: ['node_modules'],
+    extensions: ['.js', '.jsx'],
+    modules: ['src', 'node_modules'],
   },
   output: {
     library: 'react-component-starter',
     libraryTarget: 'umd',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
+        loader: 'babel-loader',
+        options: {
           cacheDirectory: true,
         },
       },
@@ -30,15 +29,22 @@ const config = {
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
   ],
+  externals: {
+    // Don't package React with the component.
+    react: 'react',
+    // If your component depends on external utility libraries like lodash,
+    // you might want to add them here.
+    // Refer to https://webpack.js.org/configuration/externals/ for more info.
+  }
 };
 
 if (env === 'production') {
   config.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
       beautify: false,
       comments: false,
       compress: {
-        warnings: false,
         drop_console: true,
       },
       mangle: {
